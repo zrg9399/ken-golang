48859508c1f652d8f2aee108a22fe64c82e840b9
zrg9399 <dianda8899>
2022-01-08 23:44:15 +0800

    提交作业

--- homework/2.1_httpServer/main.go
+++ homework/2.1_httpServer/main.go
@@ -16,6 +16,7 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", rootHandler)
 	mux.HandleFunc("/healthz", healthz)
+	//	mux.HandleFunc("/healthz", healthz)
 	err := http.ListenAndServe(":801", mux)
 	if err != nil {
 		log.Fatal(err)
@@ -23,7 +24,30 @@ func main() {
 
 }
 
+////创建一个结构体
+//type logResposeWriter struct {
+//	http.ResposeWriter1
+//	statusCode int
+//}
+//
+//func NewLogResponseWriter(w http.ResponseWriter1) *logResposeWriter {
+//	return &logResposeWriter{w, http.StatusOK}
+//}
+//func (lw *logResposeWriter) WriteHeader(code int) {
+//	lw.statusCode = code
+//	lw.ResposeWriter1.WriteHeader(code)
+//
+//}
+//func AccessLog(next http.Header) http.Header {
+//	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//		logW := NewLogResponseWriter(w)
+//		log.Println("/healthz ClientAddress is " + r.RemoteAddr + " Status Code is " + logW.statusCode)
+//	})
+//
+//}
 func healthz(w http.ResponseWriter, r *http.Request) {
+	//访问healthz返回200
+	//logW := NewLogResponseWriter(w)
 	log.Println("/healthz ClientAddress is " + r.RemoteAddr + " Status Code is " + http.StatusText(200))
 	io.WriteString(w, "/healthz ClientAddress is "+r.RemoteAddr+" Status Code is "+http.StatusText(200))
 }
@@ -36,17 +60,17 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, "hello [stranger]\n")
 	}
 	io.WriteString(w, "===================Details of the http request header:============\n")
+	//获取request的header信息并写入response中
 	for k, v := range r.Header {
 		io.WriteString(w, fmt.Sprintf("%s=%s\n", k, v))
 
 	}
-	io.WriteString(w, "===================GOROOT VERSION:============\n")
-
-	fmt.Println("GOROOT is:", os.Getenv("GOROOT"))
-	fmt.Println("GOVERSION is:", os.Getenv("GOVERSION"))
-	io.WriteString(w, os.Getenv("GOROOT"))
-	io.WriteString(w, os.Getenv("GOVERSION"))
-
+	io.WriteString(w, "=================== VERSION:============\n")
+	//获取环境变量VERSION信息
+	os.Setenv("VERSION", "1.7.3.5")
+	fmt.Println("VERSION is:", os.Getenv("VERSION"))
+	io.WriteString(w, "VERSION is:"+os.Getenv("VERSION"))
+	//记录客户端ip,http返回码、输出到server端的标准输出
 	log.Println("/ ClientAddress is " + r.RemoteAddr + " Status Code is " + http.StatusText(200))
 
 }
