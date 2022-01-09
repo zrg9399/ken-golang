1c85f9be57f7d6cfa0d5778fa9a0e204debdbead
zrg9399 <dianda8899>
2022-01-10 06:48:40 +0800

    提交作业

--- homework/2.1_httpServer/main.go
+++ homework/2.1_httpServer/main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -16,61 +17,46 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", rootHandler)
 	mux.HandleFunc("/healthz", healthz)
-	//	mux.HandleFunc("/healthz", healthz)
-	err := http.ListenAndServe(":801", mux)
+	err := http.ListenAndServe(":8081", mux)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 }
 
-////创建一个结构体
-//type logResposeWriter struct {
-//	http.ResposeWriter1
-//	statusCode int
-//}
-//
-//func NewLogResponseWriter(w http.ResponseWriter1) *logResposeWriter {
-//	return &logResposeWriter{w, http.StatusOK}
-//}
-//func (lw *logResposeWriter) WriteHeader(code int) {
-//	lw.statusCode = code
-//	lw.ResposeWriter1.WriteHeader(code)
-//
-//}
-//func AccessLog(next http.Header) http.Header {
-//	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-//		logW := NewLogResponseWriter(w)
-//		log.Println("/healthz ClientAddress is " + r.RemoteAddr + " Status Code is " + logW.statusCode)
-//	})
-//
-//}
 func healthz(w http.ResponseWriter, r *http.Request) {
 	//访问healthz返回200
-	//logW := NewLogResponseWriter(w)
-	log.Println("/healthz ClientAddress is " + r.RemoteAddr + " Status Code is " + http.StatusText(200))
-	io.WriteString(w, "/healthz ClientAddress is "+r.RemoteAddr+" Status Code is "+http.StatusText(200))
+	log.Println(" Status Code is " + http.StatusText(200))
+
 }
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("entering root handler")
-	user := r.URL.Query().Get("user")
-	if user != "" {
-		io.WriteString(w, fmt.Sprintf("hello [%s]\n", user))
-	} else {
-		io.WriteString(w, "hello [stranger]\n")
-	}
+
 	io.WriteString(w, "===================Details of the http request header:============\n")
 	//获取request的header信息并写入response中
 	for k, v := range r.Header {
 		io.WriteString(w, fmt.Sprintf("%s=%s\n", k, v))
+		for _, vv := range v {
+			fmt.Printf("Header key: %s, Header value: %s \n", k, v)
+			w.Header().Set(k, vv)
+		}
 
 	}
 	io.WriteString(w, "=================== VERSION:============\n")
 	//获取环境变量VERSION信息
 	os.Setenv("VERSION", "1.7.3.5")
+	w.Header().Get("VERSION")
 	fmt.Println("VERSION is:", os.Getenv("VERSION"))
 	io.WriteString(w, "VERSION is:"+os.Getenv("VERSION"))
 	//记录客户端ip,http返回码、输出到server端的标准输出
-	log.Println("/ ClientAddress is " + r.RemoteAddr + " Status Code is " + http.StatusText(200))
+	log.Println("/ ClientAddress is " + getCurrentIP(r) + " Status Code is " + http.StatusText(200))
 
+}
+func getCurrentIP(r *http.Request) string {
+	// 这里也可以通过X-Forwarded-For请求头的第一个值作为用户的ip ,但是要注意的是这两个请求头代表的ip都有可能是伪造的
+	ip := r.Header.Get("X-Real-IP")
+	if ip == "" {
+		//当请求头不存在即不存在代理时直接获取ip
+		ip = strings.Split(r.RemoteAddr, ":")[0]
+	}
+	return ip
 }
